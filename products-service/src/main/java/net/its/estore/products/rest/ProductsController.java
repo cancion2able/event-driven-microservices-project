3b1bca386bd27dc031974c1b5c875a0566d812a3
Stanislav Ovsyankin <sovsyankin@arlo.com>
2021-10-30 00:23:43 +0600

    [products-service] Handle HTTP methods

--- products-service/src/main/java/net/its/estore/products/rest/ProductsController.java
+++ products-service/src/main/java/net/its/estore/products/rest/ProductsController.java
@@ -1,11 +1,31 @@
 package net.its.estore.products.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
 public class ProductsController {
+
+    @PostMapping
+    public String createProduct() {
+        return "HTTP POST is handled";
+    }
+
+    @GetMapping
+    public String getProduct() {
+        return "HTTP GET is handled";
+    }
+
+    @PutMapping
+    public String updateProduct() {
+        return "Http PUT is endbled";
+    }
+
+    @DeleteMapping
+    public String deleteProduct() {
+        return "HTTP DELETE is handled";
+    }
+
 
 
 }
