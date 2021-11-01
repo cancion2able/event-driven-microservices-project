55dac4fbde8db3449cd3c79b290fcfdde9f7d85f
Stanislav Ovsyankin <sovsyankin@arlo.com>
2021-11-01 23:43:45 +0600

    [products-service] Create product entity

---
+++ products-service/src/main/java/net/its/estore/products/core/data/ProductEntity.java
@@ -0,0 +1,25 @@
+package net.its.estore.products.core.data;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@Table(name = "products")
+@Entity
+public class ProductEntity implements Serializable {
+
+    @Id
+    @Column(unique = true)
+    private String productId;
+    @Column(unique = true)
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+
+}
