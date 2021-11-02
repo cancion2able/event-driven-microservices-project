75fc1269daa5dc72f1011ec0a1b823854d476cfa
Stanislav Ovsyankin <sovsyankin@arlo.com>
2021-11-02 08:11:51 +0600

    [products-service] Implement event handler

--- products-service/src/main/java/net/its/estore/products/query/ProductEventHandler.java
+++ products-service/src/main/java/net/its/estore/products/query/ProductEventHandler.java
@@ -1,14 +1,25 @@
 package net.its.estore.products.query;
 
+import net.its.estore.products.core.data.ProductEntity;
+import net.its.estore.products.core.data.ProductRepository;
 import net.its.estore.products.core.event.ProductCreatedEvent;
 import org.axonframework.eventhandling.EventHandler;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductEventHandler {
 
+    private final ProductRepository repository;
+
+    public ProductEventHandler(ProductRepository repository) {
+        this.repository = repository;
+    }
+
     @EventHandler
     public void on(ProductCreatedEvent event) {
-
+        final ProductEntity productEntity = new ProductEntity();
+        BeanUtils.copyProperties(event, productEntity);
+        repository.save(productEntity);
     }
 }
