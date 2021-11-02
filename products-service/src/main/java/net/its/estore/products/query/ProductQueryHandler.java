ada0f0dce8e9578a0ca1afc26d49cd7bc77d474f
Stanislav Ovsyankin <sovsyankin@arlo.com>
2021-11-02 12:34:29 +0600

    [products-service] Implement products lookup

--- products-service/src/main/java/net/its/estore/products/query/ProductQueryHandler.java
+++ products-service/src/main/java/net/its/estore/products/query/ProductQueryHandler.java
@@ -1,7 +1,14 @@
 package net.its.estore.products.query;
 
+import net.its.estore.products.core.data.ProductEntity;
 import net.its.estore.products.core.data.ProductRepository;
+import net.its.estore.products.query.rest.ProductRestModel;
+import org.axonframework.queryhandling.QueryHandler;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class ProductQueryHandler {
@@ -12,5 +19,16 @@ public class ProductQueryHandler {
         this.repository = repository;
     }
 
+    @QueryHandler
+    public List<ProductRestModel> findProducts(FindProductsQuery findProductsQuery) {
+        List<ProductRestModel> productRestModels = new ArrayList<>();
+        final List<ProductEntity> storedProducts = repository.findAll();
+        for (ProductEntity storedProduct : storedProducts) {
+            final ProductRestModel productRestModel = new ProductRestModel();
+            BeanUtils.copyProperties(storedProduct, productRestModel);
+            productRestModels.add(productRestModel);
+        }
+        return productRestModels;
+    }
 
 }
