93e872174485c58399073bbdb4ab9ee11a99ef20
Stanislav Ovsyankin <sovsyankin@arlo.com>
2021-11-02 12:14:46 +0600

    [products-service] Get products rest endpoint

--- products-service/src/main/java/net/its/estore/products/query/rest/ProductQueryController.java
+++ products-service/src/main/java/net/its/estore/products/query/rest/ProductQueryController.java
@@ -1,9 +1,17 @@
 package net.its.estore.products.query.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductQueryController {
+
+    @GetMapping
+    public List<ProductRestModel> getProducts() {
+
+    }
 }
---
+++ products-service/src/main/java/net/its/estore/products/query/rest/ProductRestModel.java
@@ -0,0 +1,15 @@
+package net.its.estore.products.query.rest;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ProductRestModel {
+
+    private String productId;
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+
+}
