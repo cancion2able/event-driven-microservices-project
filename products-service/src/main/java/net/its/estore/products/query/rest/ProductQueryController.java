fa789e6e37101fb8f308447d8c1e3a107ac8409a
Stanislav Ovsyankin <sovsyankin@arlo.com>
2021-11-02 12:09:37 +0600

    [products-service] Create product query controller

---
+++ products-service/src/main/java/net/its/estore/products/query/rest/ProductQueryController.java
@@ -0,0 +1,9 @@
+package net.its.estore.products.query.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductQueryController {
+}
