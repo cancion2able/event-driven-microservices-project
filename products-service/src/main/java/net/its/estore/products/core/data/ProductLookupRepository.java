3df1186e31d2efcef4f114a0f43993fc15d68da7
Stanislav Ovsyankin <sovsyankin@arlo.com>
2021-11-05 11:24:16 +0600

    [products-service] Create product lookup repository

---
+++ products-service/src/main/java/net/its/estore/products/core/data/ProductLookupRepository.java
@@ -0,0 +1,8 @@
+package net.its.estore.products.core.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductLookupRepository extends JpaRepository<ProductLookupEntity, String> {
+
+    ProductLookupEntity findByProductIdOrTitle(String productId, String title);
+}
