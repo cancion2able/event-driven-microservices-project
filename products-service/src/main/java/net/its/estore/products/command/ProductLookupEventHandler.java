ed9a59b9da89554d12c7794b6c3d2a2dc42e7177
Stanislav Ovsyankin <sovsyankin@arlo.com>
2021-11-06 16:20:44 +0600

    [products-service] Persistent info into a ProductLookup table

--- products-service/src/main/java/net/its/estore/products/command/ProductLookupEventHandler.java
+++ products-service/src/main/java/net/its/estore/products/command/ProductLookupEventHandler.java
@@ -1,5 +1,7 @@
 package net.its.estore.products.command;
 
+import net.its.estore.products.core.data.ProductLookupEntity;
+import net.its.estore.products.core.data.ProductLookupRepository;
 import net.its.estore.products.core.event.ProductCreatedEvent;
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
@@ -9,8 +11,15 @@ import org.springframework.stereotype.Component;
 @ProcessingGroup("product-group")
 public class ProductLookupEventHandler {
 
+    private final ProductLookupRepository repository;
+
+    public ProductLookupEventHandler(ProductLookupRepository repository) {
+        this.repository = repository;
+    }
+
     @EventHandler
     public void on(ProductCreatedEvent event) {
-
+        final ProductLookupEntity productLookupEntity = new ProductLookupEntity(event.getProductId(), event.getTitle());
+        repository.save(productLookupEntity);
     }
 }
