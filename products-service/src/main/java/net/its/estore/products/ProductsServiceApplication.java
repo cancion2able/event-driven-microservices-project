cd2ad543b5334efabe708473968b7f98a2e94246
Stanislav Ovsyankin <sovsyankin@arlo.com>
2021-11-10 13:27:18 +0600

    [products-service] Register ListenerInvocationErrorHandler

--- products-service/src/main/java/net/its/estore/products/ProductsServiceApplication.java
+++ products-service/src/main/java/net/its/estore/products/ProductsServiceApplication.java
@@ -1,7 +1,9 @@
 package net.its.estore.products;
 
 import net.its.estore.products.command.interceptor.CreateProductCommandInterceptor;
+import net.its.estore.products.core.handler.ProductEventErrorHandler;
 import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.config.EventProcessingConfigurer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,6 +21,11 @@ public class ProductsServiceApplication {
     @Autowired
     public void registerCreateProductCommandInterceptor(ApplicationContext context, CommandBus commandBus) {
         commandBus.registerDispatchInterceptor(context.getBean(CreateProductCommandInterceptor.class));
+    }
+
+    @Autowired
+    public void configure(EventProcessingConfigurer config) {
+        config.registerListenerInvocationErrorHandler("product-group", conf -> new ProductEventErrorHandler());
     }
 
 }
