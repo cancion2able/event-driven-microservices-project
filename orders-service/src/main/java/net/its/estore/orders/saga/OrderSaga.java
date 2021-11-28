d6d3d1e6a785bda56a353b6c42bca2854ad37f0f
Stanislav Ovsyankin <sovsyankin@arlo.com>
2021-11-28 16:27:50 +0600

    [orders-service] Publish reserve product command

--- orders-service/src/main/java/net/its/estore/orders/saga/OrderSaga.java
+++ orders-service/src/main/java/net/its/estore/orders/saga/OrderSaga.java
@@ -1,7 +1,11 @@
 package net.its.estore.orders.saga;
 
 import lombok.RequiredArgsConstructor;
+import net.its.estore.core.command.ReserveProductCommand;
 import net.its.estore.orders.core.event.OrderCreatedEvent;
+import org.axonframework.commandhandling.CommandCallback;
+import org.axonframework.commandhandling.CommandMessage;
+import org.axonframework.commandhandling.CommandResultMessage;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.modelling.saga.SagaEventHandler;
 import org.axonframework.modelling.saga.StartSaga;
@@ -16,6 +20,16 @@ public class OrderSaga {
     @StartSaga
     @SagaEventHandler(associationProperty = "orderId")
     public void handle(OrderCreatedEvent event) {
-
+        final ReserveProductCommand reserveProductCommand = ReserveProductCommand.builder()
+                .orderId(event.getOrderId())
+                .productId(event.getProductId())
+                .quantity(event.getQuantity())
+                .userId(event.getUserId())
+                .build();
+        commandGateway.send(reserveProductCommand, (commandMessage, commandResultMessage) -> {
+            if (commandResultMessage.isExceptional()) {
+                // start a compensating transaction
+            }
+        });
     }
 }
