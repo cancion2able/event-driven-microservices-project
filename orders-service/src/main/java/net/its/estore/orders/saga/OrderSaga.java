a22bd60a3a402387c49169cf73804ec47828b504
Stanislav Ovsyankin <sovsyankin@arlo.com>
2021-11-28 16:07:26 +0600

    [orders-service] Create saga event handler

--- orders-service/src/main/java/net/its/estore/orders/saga/OrderSaga.java
+++ orders-service/src/main/java/net/its/estore/orders/saga/OrderSaga.java
@@ -1,7 +1,10 @@
 package net.its.estore.orders.saga;
 
 import lombok.RequiredArgsConstructor;
+import net.its.estore.orders.core.event.OrderCreatedEvent;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.modelling.saga.SagaEventHandler;
+import org.axonframework.modelling.saga.StartSaga;
 import org.axonframework.spring.stereotype.Saga;
 
 @Saga
@@ -9,4 +12,11 @@ import org.axonframework.spring.stereotype.Saga;
 public class OrderSaga {
 
     private final transient CommandGateway commandGateway;
+
+    @StartSaga
+    @SagaEventHandler(associationProperty = "orderId")
+    public void handle(OrderCreatedEvent event) {
+
+
+    }
 }
